673b8eb4208c497407f04d1c981c9c4c6ad8265e
arcoal2002 <arcoal2002@gmail.com>
2023-12-14 15:47:05 -0600

    [A]First commit front(vue,tailwind) y back(Express)

---
+++ front/induccion/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="justify-center flex bg-yellow-300 items-center h-screen">
+    <div class="text-4xl">
+      Hello 👋🏼
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
