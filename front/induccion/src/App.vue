5e50441ce4f8e0110b8dbab5df765dc74ae78511
arcoal2002 <arcoal2002@gmail.com>
2023-12-22 18:10:59 -0600

    [A]Header creado y empezado

--- front/induccion/src/App.vue
+++ front/induccion/src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <HeaderComp></HeaderComp>
   <router-view/>
 </template>
+<script>
+import HeaderComp from './components/HeaderComp.vue';
+
+export default {
+  components:{
+    HeaderComp
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ front/induccion/src/components/HeaderComp.vue
@@ -0,0 +1,57 @@
+<template>
+    <!-- En cuanto a la altura no se que estaría bien así que lo dejo a tu criterio -->
+    <div class="w-full flex text-white ">
+        <div class="w-1/2 lg:w-1/4  ">
+            <!-- Aqui va a ir el logo del tec para que dejes el espacio y ya lo pongas como te parezca -->
+        </div>
+        <nav class="w-1/2 lg:w-3/4 flex bg-blue-500 place-content-end">
+            <!-- Aqui son los elementos -->
+            <div class="lg:hidden">
+                <p>Menu</p>
+                <!-- Aquí se hará una nav var del tipo lateral para movil -->
+            </div>
+            <div class="hidden lg:flex">
+                <!-- Esta es una nav var para computadora -->
+                <div v-for="section in page_data" :key="section.id" class="px-5">
+                    <!-- Este boton redirecciona a otras paginas -->
+                    <div v-if="section.type == 'page'">
+                        <div class="w-10">
+                            <p>{{ section.title }}</p>
+                        </div>
+                    </div>
+                    <!-- Este de aqui redirecciona a un menu desplegable -->
+                    <div v-else>
+                        <button>
+                            {{ section.title }}
+                        </button>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            page_data: [
+                {
+                    "title": "Inicio",
+                    "type": "page",
+                    "data": ""
+                },
+                {
+                    "title": "Departamentos",
+                    "type": "object",
+                    "data": []
+                },
+
+            ]
+        }
+    },
+}
+</script>
